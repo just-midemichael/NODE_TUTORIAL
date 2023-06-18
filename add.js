5d97c1787131cc690d24e26238fa46e4d1ce763b
Olumide Michael <olumide.m.ojo@gmail.com>
2023-06-18 21:11:19 +0100

    addition function

---
+++ add.js
@@ -0,0 +1,16 @@
+const add = (a, b) => {
+    return a + b;
+};
+
+const multiply = (x, y) => {
+
+}
+
+// This makes use of the add fuction by specifying value 
+// of the aguments a & b 
+// const sum = add(3, 5); 
+// console.log(sum);
+
+// exports multiply function using module dot exports object
+module.exports =  add;
+
