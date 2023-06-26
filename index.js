923d27d2716df1115fe919ead3e1f97d9dbb3383
Olumide Michael <olumide.m.ojo@gmail.com>
2023-06-26 11:01:30 +0100

    .

--- index.js
+++ index.js
@@ -450,3 +450,23 @@
 //         console.log(`Hash: ${i - 1}`, Date.now() - start);
 //     });
 // };
+
+
+// lesson 12
+// Network i/o
+const https = require("node:https");
+
+const MAX_CALLS = 20;
+
+const start = Date.now();
+
+for(let i = 0; i < MAX_CALLS; i++ ) {
+    https
+        .request("https://www.google.com", (res) => {
+            res.on("data", () => {});
+            res.on("end", () => {
+                console.log(`Request: ${i - 1}`, Date.now() - start);
+            });
+        })
+        .end();
+};
