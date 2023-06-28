89e758d3e4171841137bf625dc8f51b703c279f3
Olumide Michael <olumide.m.ojo@gmail.com>
2023-06-28 13:31:00 +0100

    feat: CLI options, pokemon, pokemonName, moves, slice(0, 2), npm install yargs, npm install -g, async, await, fetch, res.json

--- my-custom-cli/index.js
+++ my-custom-cli/index.js
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
 
-console.log("olumide pokedex");
+const yargs = require("yargs");
+const {argv} = yargs(process.argv);
+
+// console.log("olumide pokedex");
+
+const arrayStart = 0;
+const arrayEnd = 5;
+
+const printFiveMoves = async (pokemonName) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    const pokemon = await response.json();
+    const moves = pokemon.moves.map(({move}) => move.name)
+    console.log(moves.slice(arrayStart, arrayEnd))
+};
+
+printFiveMoves(argv.pokemon);
