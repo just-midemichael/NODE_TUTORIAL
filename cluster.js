39b9819b85bfd2a204094d89e32f48b406252ad8
Olumide Michael <olumide.m.ojo@gmail.com>
2023-06-29 13:18:26 +0100

    Cluster Module, npm install -g pm2, pm2 start/pm2 stop;  no-cluster-module -i 0, 'node:os', 'node:http'

---
+++ cluster.js
@@ -0,0 +1,31 @@
+const cluster = require("node:cluster");
+const http = require("node:http");
+const OS = require("node:os");
+
+console.log(OS.cpus().length, "cpu cores are in this local machine");
+
+if(cluster.isMaster) {
+    console.log(`Master Process ${process.pid} is running`);
+    cluster.fork();// First Worker
+    cluster.fork();// Second Worker
+}
+else {
+    console.log(`Worker ${process.pid} started`);
+    
+    const server = http.createServer((res, req) => {
+        
+        if(req.url === ("/")) {
+            res.writeHead(200, {"Content-Type": "text/plain"});
+            res.end("Home Page");
+        }// First Worker
+        else if (req.url === "/slow-page") {
+            for (let i = 0; i < 600000000; i++) {} // Simulate CPU work
+            res.writeHead(200, {"Content-Type": "text/plain"});
+            res.end("Slow Page");
+        }// Second Worker
+    });
+    const port = 8000;
+    const printMessage = `Server is running on port ${port}` 
+    
+    server.listen(port, () => console.log(printMessage));
+};
---
+++ dist/no-cluster.dev.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var http = require("node:http");
+
+var port = 8000;
+var printMessage = "Server is running on port ".concat(port);
+var server = http.createServer(function (res, req) {
+  if (req.url === "/") {
+    res.writeHead(200, {
+      "Content-Type": "text/plain"
+    });
+    res.end("Home Page");
+  } else if (req.url === "/slow-page") {
+    for (var i = 0; i < 600000000; i++) {} // Simulate CPU work
+
+
+    res.writeHead(200, {
+      "Content-Type": "text/plain"
+    });
+    res.end("Slow Page");
+  }
+});
+server.listen(8000, function () {
+  return console.log(printMessage);
+});
